7f2fa5f594d55c3a4ef9043f7a1dc18441733393
refreezer1 <235122258+refreezer1@users.noreply.github.com>
2025-10-21 09:47:04 +0200

    refactor: js exports for node.js esm

--- index.ts
+++ index.ts
@@ -2,13 +2,13 @@
 export {
   formatNumberCompact,
   formatNumberFull,
-} from './formatters/number-format/number-format'
+} from './formatters/number-format/number-format.js'
 
 // Currency formatters
 export {
   formatCurrencyCompact,
   formatCurrencyFull,
-} from './formatters/currency-format/currency-format'
+} from './formatters/currency-format/currency-format.js'
 
 // Percentage formatters
-export { formatPercentage } from './formatters/percentages-format/percentages-format'
+export { formatPercentage } from './formatters/percentages-format/percentages-format.js'
