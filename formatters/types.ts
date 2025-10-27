4b55e1ac54dd39bee0a80399abaefc9db9cc4825
refreezer1 <235122258+refreezer1@users.noreply.github.com>
2025-10-27 20:15:02 +0100

    refactor: typing FormatNumberOptions

---
+++ formatters/types.ts
@@ -0,0 +1,9 @@
+/**
+ * Options for formatting numbers.
+ */
+export type FormatNumberOptions = {
+  /** Minimum number of decimal places to display (default: 0) */
+  minimumFractionDigits: number
+  /** Maximum number of decimal places to display (default: 2) */
+  maximumFractionDigits: number
+}
